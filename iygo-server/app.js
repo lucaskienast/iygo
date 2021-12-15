302665f12d69543a082c505f15858b170d665a59
LucasKienast <lucas.kienast@hotmail.de>
2021-12-15 16:44:26 +0000

    heroku security prep

--- iygo-server/app.js
+++ iygo-server/app.js
@@ -4,6 +4,11 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const connectDB = require('./db/connect.js');
 const cardsRouter = require('./routes/cards.js');
 const authRouter = require('./routes/auth.js');
@@ -12,7 +17,15 @@ const notFoundMiddleware = require('./middleware/not-found.js');
 const errorHandlerMiddleware = require('./middleware/error-handler.js');
 
 // middleware
+app.set('trust proxy', 1); // for heroku deployment
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.get('/', (req, res) => {
