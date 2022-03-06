3c98052f65f24ef37063e2c97ccbee6e18a6e4e3
LucasKienast <lucas.kienast@hotmail.de>
2022-03-06 13:11:04 +0100

    YPL-168: added sidebar

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <Wrapper>
       <h5>
         &copy; {new Date().getFullYear()}
-        <span>YGO Phoenix League</span>
+        <span> YGO Phoenix League</span>
       </h5>
       <h5>All rights reserved.</h5>
     </Wrapper>
