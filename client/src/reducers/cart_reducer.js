0c1f2f9db51e469caaa424d288de7024b60b2421
LucasKienast <lucas.kienast@hotmail.de>
2022-03-15 23:47:33 +0100

    YPL-180: added deck reducer for new cards

--- client/src/reducers/cart_reducer.js
+++ client/src/reducers/cart_reducer.js
@@ -1,5 +1,5 @@
 import {
-  ADD_TO_CART,
+  ADD_TO_DECK,
   CLEAR_CART,
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
@@ -7,7 +7,23 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
-  return state
+
+  if (action.type === ADD_TO_DECK) {
+    const {card, amount} = action.payload;
+    const tempItem = state.deck.find((c) => {
+      return c.card_id === card.card_id;
+    });
+    if (tempItem) {
+
+    } else {
+      const newItem = {
+        card,
+        amount
+      };
+      return {...state, deck: [...state.deck, newItem]};
+    }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
